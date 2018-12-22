20d7f7580d9054fdbe1631a103e84db075e26944
Demetra Staykova <demschoy@gmail.com>
2018-12-22 15:40:06 +0200

    added not quite working POST method

--- com/fmi/mpr/hw/http/81524.java
+++ com/fmi/mpr/hw/http/81524.java
@@ -1,5 +1,4 @@
-//package com.fmi.mpr.hw.http;
-package last;
+package com.fmi.mpr.hw.http;
 
 import java.net.*;
 
@@ -11,47 +10,19 @@ public class HttpServer
 	private String type;
 	private String filename;
 	private boolean isRunning;
-	private final String errorImage = "<!DOCTYPE html>\n" + 
+	private final String errorMessage = "<!DOCTYPE html>\n" + 
 			   "<html>\n" + 
 			   "<head>\n" + 
 			   "	<title></title>\n" + 
 			   "</head>\n" + 
 			   "<body>\n" +
-			   "<form action=\"/action_page.php\">\n" + 
-			   "			  <input type=\"file\" name=\"pic\" accept=\"image/*\">\n" + 
-			   "			  <input type=\"submit\">\n" + 
-			   "			</form> " +
+			   			"ERROR! The file you're requesting for cannot be found. Please try again." +
 			   "</body>\n" + 
 			   "</html>";
-	
-	private final String errorVideo = "<!DOCTYPE html>\n" + 
-			   "<html>\n" + 
-			   "<head>\n" + 
-			   "	<title></title>\n" + 
-			   "</head>\n" + 
-			   "<body>\n" +
-			   "<form action=\"/action_page.php\">\n" + 
-			   "			  <input type=\"file\" name=\"video\" accept=\"video/*\">\n" + 
-			   "			  <input type=\"submit\">\n" + 
-			   "			</form> " +
-			   "</body>\n" + 
-			   "</html>";
-	
-	private final String errorText = "<!DOCTYPE html>\n" + 
-			   "<html>\n" + 
-			   "<head>\n" + 
-			   "	<title></title>\n" + 
-			   "</head>\n" + 
-			   "<body>\n" +
-			   "<form action=\"/action_page.php\">\n" + 
-			   "			  <input type=\"file\" name=\text\" accept=\"text/*\">\n" + 
-			   "			  <input type=\"submit\">\n" + 
-			   "			</form> " +
-			   "</body>\n" + 
-			   "</html>";
+		
 	public HttpServer() throws IOException
 	{
-		this.server = new ServerSocket(8800);
+		this.server = new ServerSocket(8801);
 	}
 	
 	public void start()
@@ -103,19 +74,31 @@ public class HttpServer
 				BufferedInputStream bis = new BufferedInputStream(client.getInputStream());
 				PrintStream ps = new PrintStream(client.getOutputStream(),true))
 			{
-				String response = read(ps, bis);
+				String response = read(ps, client, bis);
 				writePOST(ps, response);
 			}
 	}
 	
 	private void writePOST(PrintStream ps, String response) throws IOException //POST
 	{
-		if(ps != null)
-		{}
+		if (ps != null) 
+		{
+			//ps.println("HTTP/1.1 200 OK");
+			ps.println();
+			ps.println( "<!DOCTYPE html>\n" + 
+						"<form action=\"upload.php\" method=\"POST\" enctype=\"multipart/form-data\">\n" + 
+						"Select file to upload:\n" +
+						"<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">\n" +
+						"<input type=\"submit\" value=\"Upload File\" name=\"submit\">\n" +
+						"</form>" +
+					"<h2>" + (response == null || response.trim().isEmpty() ? "" : response) + "</h2>" +
+					"</body>\n" + 
+					"</html>");
+		}
 	}
 	
 	
-	private String read(PrintStream ps, BufferedInputStream bis) throws Exception
+	private String read(PrintStream ps, Socket client, BufferedInputStream bis) throws Exception
 	{
 		if(bis != null)
 		{
@@ -132,13 +115,13 @@ public class HttpServer
 						break;
 			}
 		
-			return parseRequest(ps, request.toString());
+			return parseRequest(ps, client, request.toString());
 		}
 		
 		return "Error";
 	}
 	
-	private String parseRequest(PrintStream ps, String request) throws Exception
+	private String parseRequest(PrintStream ps, Socket client, String request) throws Exception
 	{
 		String[] lines = request.split("\n");
 		String header = lines[0];
@@ -146,15 +129,19 @@ public class HttpServer
 		String[] headerParts = header.split(" ");
 		this.type = headerParts[0];
 		
-		if(type.equals("GET"))
-			parseGetRequest(ps, request);
-		if(type.equals("POST"))
-			parsePostRequest(ps, request);
+		String fullFileName = headerParts[1];
 		
-		return null;
+		if(type.equals("GET"))
+			return parseGetRequest(ps, request);
+		if(type.equals("POST"))
+			return parsePostRequest(ps, lines, fullFileName, client);
+		
+		return parsePostRequest(ps, lines, fullFileName, client);
+		
+		//return null;
 	}
 
-	private void parseGetRequest(PrintStream ps, String request) throws Exception 
+	private String parseGetRequest(PrintStream ps, String request) throws Exception 
 	{
 		ps.println("HTTP/1.1 200 OK");
 		
@@ -172,32 +159,15 @@ public class HttpServer
 				sendImage(ps);
 				System.out.println("Sent image.");
 			}
-		}
-		catch(Exception e) 
-		{
-			ps.println(errorImage);
-		}
-		
-		try
-		{
-
+			
 			if(extension.equals("mp4") || extension.equals("avi"))
 			{
 				ps.println("Content-Type: video/mp4");
 				ps.println();
 				sendVideo(ps);
 				System.out.println("Sent video.");
-				
 			}
-		}
-		catch(Exception e) 
-		{
-			ps.println(errorVideo);
-		}
-		
 			
-		try
-		{	
 			if(extension.equals("txt"))
 			{
 				ps.println();
@@ -207,8 +177,10 @@ public class HttpServer
 		}
 		catch(Exception e)
 		{
-			ps.println(errorText);
+			ps.println(errorMessage);
 		}
+		
+		return null;
 	}
 	
 	private void sendText(PrintStream ps) throws IOException 
@@ -253,16 +225,89 @@ public class HttpServer
 		ps.flush();
 		fileIn.close();
 	}
-	private String parsePostRequest(PrintStream ps, String request) throws IOException 
+	
+	private String parsePostRequest(PrintStream ps, String[] lines, String filename, Socket client) throws IOException 
 	{		
-		return null;
+		StringBuilder body = new StringBuilder();
+		boolean readBody = false;
+		for (String line : lines) {
+			if (readBody) {
+				body.append(line);
+			}
+			if (line.trim().isEmpty()) {
+				readBody = true;
+			}
+		}
+		
+		return parseBody(client, body.toString());
 	}
 
-	private String parseTextBody(String body) 
+	private String parseBody(Socket client, String body) throws IOException 
 	{
+		if (body != null && !body.trim().isEmpty()) 
+		{
+			String[] operands = body.split("&");
+			String fileName = operands[0].split("=")[1];
+			
+			return sendFile(fileName, body, client);
+		}
 		return null;
 	}
 	
+	private String sendFile(String fileName, String body, Socket client) throws IOException 
+	{
+		BufferedInputStream bis = new BufferedInputStream(client.getInputStream());
+		String extension = fileName.split("\\.")[1];
+		String data = null;
+		PrintStream ps = new PrintStream(client.getOutputStream(), true);
+		
+		if(extension.equals("png") || extension.equals("jpg") || extension.equals("jpeg") || extension.equals("bmp")
+				|| extension.equals("mp4") || extension.equals("avi"))
+		{
+			data = sendMedia(bis, ps);
+		}
+		
+		if(extension.equals("txt"))
+		{
+			data = sendTextFiles(bis, ps);
+		}
+        
+		File statText = new File(fileName);
+        FileOutputStream is = new FileOutputStream(statText);
+        OutputStreamWriter osw = new OutputStreamWriter(is);    
+        Writer w = new BufferedWriter(osw);
+        w.write(data);
+        w.close();
+        
+		return null;
+	}
+
+	private String sendTextFiles(BufferedInputStream bis, PrintStream ps) throws IOException 
+	{
+		ps.flush();
+		int bytesRead = 0;
+		byte[] buffer = new byte[8192];
+	
+		while((bytesRead = bis.read(buffer, 0, 8192)) > 0)
+			ps.write(buffer, 0, bytesRead);
+		
+		return ps.toString();
+		
+	}
+
+	private String sendMedia(BufferedInputStream bis, PrintStream ps) throws IOException 
+	{
+		ps.flush();
+		int bytesRead = 0;
+		byte[] buffer = new byte[8192];
+	
+		while((bytesRead = bis.read(buffer, 0, 8192)) > 0)
+			ps.write(buffer, 0, bytesRead);
+		
+		return ps.toString();
+		
+	}
+
 	public static void main(String[] args) throws IOException
 	{
 		HttpServer serv = new HttpServer();
